d3a41d339de8a88cc5965207f3064883c8a432f5
ksh03003 <74225015+kwakchaewon@users.noreply.github.com>
2024-01-30 22:28:08 +0900

    JWT 설정 및 보안필터 정의

--- review/src/main/java/com/example/review/config/SecurityConfig.java
+++ review/src/main/java/com/example/review/config/SecurityConfig.java
@@ -1,23 +1,77 @@
 package com.example.review.config;
 
+import com.example.review.jwt.JwtAccessDeniedHandler;
+import com.example.review.jwt.JwtAuthenticationEntryPoint;
+import com.example.review.jwt.JwtSecurityConfig;
+import com.example.review.jwt.TokenProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
 
 @EnableWebSecurity
 @Configuration
-public class SecurityConfig {
+@EnableMethodSecurity(prePostEnabled = true)
+public class SecurityConfig{
+
+    private final TokenProvider tokenProvider;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+
+    public SecurityConfig(TokenProvider tokenProvider,
+                          JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint,
+                          JwtAccessDeniedHandler jwtAccessDeniedHandler) {
+        this.tokenProvider = tokenProvider;
+        this.jwtAuthenticationEntryPoint = jwtAuthenticationEntryPoint;
+        this.jwtAccessDeniedHandler = jwtAccessDeniedHandler;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeRequests() // 스프링 시큐리티 구성 메서드로 HTTP 요청 인가 설정. HttpServeletRequest 사용 요청에 대한 접근 제한 설정
-                .requestMatchers("/hello").permitAll() // 특정 매처를 지정해 특정 경로나 URL 패턴에 대한 인가 규칙을 설정 EX) /admin/**:
-                .anyRequest().authenticated();
+                // csrf disable
+//                .csrf((csrf) -> csrf
+//                        .ignoringRequestMatchers(new AntPathRequestMatcher("**")))
+                .csrf(AbstractHttpConfigurer::disable)
 
+                // exceptionHandling
+                .exceptionHandling((exceptionHandling)->exceptionHandling
+                        .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                        .accessDeniedHandler(jwtAccessDeniedHandler)
+                )
+
+                // h2 console 설정
+//                .headers((headers)->headers
+//                        .frameOptions()
+//                        .sameOrigin()
+//                )
+
+                // sessionManagement: 세션 사용하지 않음
+                .sessionManagement((sessionManagement)->sessionManagement
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+
+                // authorizeHttpRequests: 토큰 없는 URL 설정
+                .authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests
+//                        .requestMatchers("/api/hello", "/api/authenticate", "/api/signup").permitAll()
+//                        .requestMatchers(new AntPathRequestMatcher("/hello")).permitAll()
+                        .requestMatchers("/hello").permitAll()
+                        .anyRequest().authenticated()
+                )
+
+                .apply(new JwtSecurityConfig(tokenProvider));
         return http.build();
     }
 
