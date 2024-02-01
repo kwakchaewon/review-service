963c5d047d8176d2f6c9b6c0583b9bc18435a49f
ksh03003 <74225015+kwakchaewon@users.noreply.github.com>
2024-02-01 18:55:45 +0900

    Update Authority.java

--- review/src/main/java/com/example/review/model/Authority.java
+++ review/src/main/java/com/example/review/model/Authority.java
@@ -4,10 +4,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Entity
 @Table(name = "authority")
@@ -15,6 +12,7 @@ import lombok.Setter;
 @Setter
 @Builder
 @AllArgsConstructor
+@NoArgsConstructor
 public class Authority {
     @Id
     @Column(name = "authority_name", length = 50)
